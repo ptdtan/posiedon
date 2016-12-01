67db524fe42c6dcbfa303484e12a3ab0f3b05624
Phan Thai Duy tan <ptdtan@gmail.com>
2016-12-01 17:45:18 +0700

    update

--- src/rdis.c
+++ src/rdis.c
@@ -9,13 +9,17 @@
 #include <stdio.h>
 #include "../minilib/bam.h"
 
+#ifndef DEBUG
+#define DEBUG(l,i) printf("l: %d, i: %d\n", l, i);
+#endif
+
 #ifndef LOOP
-#define LOOP(i,s,e) for(int i = s; i < e; i++)
+#define LOOP(i,s,e) for(i = s; i < e; i++)
 #endif
 
 #ifndef SHIFT_LEFT
 #define SHIFT_LEFT(cigar,l) \
-  memmove(&cigar[1], &cigar[0], (l-1)*sizeof(unsigned int); \
+  memmove(&cigar[0], &cigar[1], (l-1)*sizeof(unsigned int)); \
   --l;
 #endif
 
@@ -26,30 +30,27 @@
 #endif
 
 #ifndef MERGE_SOFTCLIP_LEFT
-#define MERGE_SOFTCLIP_LEFT(cigar, l, pos, e_dis) \
-  unsigned int l_clip = cigar[0]>>BAM_CIGAR_SHIFT; \
-  unsigned int l_match = cigar[1]>>BAM_CIGAR_SHIFT; \
-  unsigned int m_match = l_clip + l_match; \
-  cigar[1] = bam_cigar_gen(m_match, bam_cigar_op('M')); \
+#define MERGE_SOFTCLIP_LEFT(cigar, l, pos, l_clip, l_match) \
+  l_clip = cigar[0]>>BAM_CIGAR_SHIFT; \
+  l_match = cigar[1]>>BAM_CIGAR_SHIFT; \
+  cigar[1] = bam_cigar_gen(l_clip + l_match, BAM_CMATCH); \
   SHIFT_LEFT(cigar, l); \
-  pos -= l_clip; \
-  e_dis += l_clip;
+  pos -= l_clip;
 #endif
 
 #ifndef MERGE_SOFTCLIP_RIGHT
-#define MERGE_SOFTCLIP_RIGHT(cigar, l, e_dis) \
-  unsigned int l_clip = cigar[l-1]>>BAM_CIGAR_SHIFT; \
-  unsigned int l_match = cigar[l-2]>>BAM_CIGAR_SHIFT; \
-  unsigned int m_match = l_clip + l_match; \
-  cigar[l-2] = bam_cigar_gen(m_match, BAM_CMATCH); \
-  e_dis += l_clip; \
+#define MERGE_SOFTCLIP_RIGHT(cigar, l, l_clip, l_match) \
+  l_clip = cigar[l-1]>>BAM_CIGAR_SHIFT; \
+  l_match = cigar[l-2]>>BAM_CIGAR_SHIFT; \
+  cigar[l-2] = bam_cigar_gen(l_clip + l_match, BAM_CMATCH); \
   cigar[l-1] = 0; \
   --l;
 #endif
 
 #ifndef INDEL_AT_SPLICE
-#define INDEL_AT_SPLICE(cigar, flag, l) \
-  LOOP(int i, 0, l-1){ \
+#define INDEL_AT_SPLICE(cigar, flag, l, i) \
+  flag = 0; \
+  LOOP(i, 0, l-1){ \
     if(((bam_cigar_opchr(cigar[i])=='I' | bam_cigar_opchr(cigar[i])=='D' ) && bam_cigar_opchr(cigar[i+1])=='N')\
         | (bam_cigar_opchr(cigar[i])=='N' && (bam_cigar_opchr(cigar[i+1])=='I' | bam_cigar_opchr(cigar[i+1])=='D'))) \
       { flag++; \
@@ -60,22 +61,18 @@
 #ifndef INDEL_RIGHT
 #define INDEL_RIGHT(cigar, l, i, t, l_id) \
   l_id = 0; \
-  int j; \
-  LOOP(j, i, l-1){ \
-    if(bam_cigar_opchr(cigar[j])=='M' && (bam_cigar_opchr(cigar[j+1])=='I' | bam_cigar_opchr(cigar[j+1])=='D')) \
-      { t = bam_cigar_opchr(cigar[j+1]); \
-        l_id = cigar[j]>>BAM_CIGAR_SHIFT;\
+  LOOP(i, 0, l-1){ \
+    if(bam_cigar_opchr(cigar[i])=='M' && (bam_cigar_opchr(cigar[i+1])=='I' | bam_cigar_opchr(cigar[i+1])=='D')) \
+      { t = bam_cigar_opchr(cigar[i+1]); \
+        l_id = cigar[i+1]>>BAM_CIGAR_SHIFT;\
         break; }\
-        i = j;\
   }
 #endif
 
 #ifndef MERGE_INDEL
-#define MERGE_INDEL(cigar, l, e_dis) \
-  unsigned int l_indel = 0; \
-  char tmp_c = '\0'; \
-  int i = 0;\
-  while(true) { \
+#define MERGE_INDEL(cigar, l, i, l_indel, tmp_c) \
+  l_indel = 0; \
+  while(1) { \
     INDEL_RIGHT(cigar, l, i, tmp_c, l_indel) \
     if(!l_indel) \
       break; \
@@ -84,8 +81,8 @@
       cigar[i+1] = bam_cigar_gen(l_indel, BAM_CMATCH); \
       SHIFT_NLEFT(cigar, l, i+1); \
     } else if(tmp_c=='I'){ \
-        cigar[i+1] = bam_cigar_gen(l_indel, BAM_CMATCH); \
-        SHIFT_NLEFT(cigar, l, i+1); \
+        cigar[i+1] = cigar[i]; \
+        SHIFT_NLEFT(cigar, l, i+1) \
     } else {\
       fprintf(stderr, "Didn't reconigze BAM CIGAR character %s", &tmp_c); \
       exit(EXIT_FAILURE); \
@@ -94,11 +91,11 @@
 #endif
 
 #ifndef MERGE_MATCH
-#define MERGE_MATCH(cigar, l, i) \
-  unsigned l_m1 = cigar[i]>>BAM_CIGAR_SHIFT; \
-  unsigned l_m2 = cigar[i+1]>>BAM_CIGAR_SHIFT; \
-  cigar[i+1] = bam_cigar_gen(l_m1+l_m2, BAM_CMATCH)\
-  SHIFT_NLEFT(cigar, l, i+1);
+#define MERGE_MATCH(cigar, l, i, l_m1, l_m2) \
+  l_m1 = cigar[i]>>BAM_CIGAR_SHIFT; \
+  l_m2 = cigar[i+1]>>BAM_CIGAR_SHIFT; \
+  cigar[i+1] = bam_cigar_gen(l_m1+l_m2, BAM_CMATCH);\
+  SHIFT_NLEFT(cigar, l, i+1)
 #endif
 
 #ifndef CONSECUTIVE_MATCH
@@ -113,26 +110,40 @@
 #endif
 
 #ifndef MERGE_MATCH_ITER
-#define MERGE_MATCH_ITER(cigar, l) \
-  int i=0, flag; \
-  while(true){ \
-    CONSECUTIVE_MATCH(cigar, l, i, flag); \
-    if(flag){ \
-      MERGE_MATCH(cigar, l , i)\
+#define MERGE_MATCH_ITER(cigar, l, i, l_m1, l_m2, flag) \
+  while(1){ \
+    CONSECUTIVE_MATCH(cigar, l, i, flag) \
+    if(flag){\
+      MERGE_MATCH(cigar, l , i, l_m1, l_m2) \
     } else break; \
 }
 #endif
 
+#ifndef EDIT_DISTANCE
+#define EDIT_DISTANCE(cigar, l, i, e_dis) \
+  LOOP(i, 0, l){ \
+    if(bam_cigar_opchr(cigar[i])=='S' | bam_cigar_opchr(cigar[i])=='D' | bam_cigar_opchr(cigar[i])=='I') \
+      e_dis+=cigar[i]>>BAM_CIGAR_SHIFT;\
+  }
+#endif
+
 #ifndef DEBUG_CIGAR
-#define DEBUG_CIGAR(cigar, l) \
-  int i; \
-  unsigned int cigar_c; \
+#define DEBUG_CIGAR(cigar, l, i, cigar_c) \
   LOOP(i, 0, l){ \
     cigar_c = bam_cigar_opchr(cigar[i]); \
     printf("%d%s", cigar[i]>>BAM_CIGAR_SHIFT, &cigar_c); \
     } \
   printf("\n");
 #endif
+
+#ifndef FIND_JUNCTIONS
+#define FIND_JUNCTIONS(cigar, l, i, js, jidx, pos) \
+  LOOP(i,0,l){ \
+    if(bam_cigar_opchr(cih))\
+}
+
+#endif
+
 
 #ifndef MAX_SPLIT_AROUND
 #define MAX_SPLIT_AROUND 200
@@ -189,20 +200,58 @@ void fill_bucket(bam1_t *read, Reg *region, Bucket *bucket){
 }
 
 int main(int argc, char *argv[]){
-  int beg = atoi(argv[1]);
-  int end = atoi(argv[2]);
-  int tid = 20, it, c_i;
-  unsigned int *cigar;
+  //int beg = atoi(argv[1]);
+  //int end = atoi(argv[2]);
+  int tid = 20, it, c_i, i, flag, l_id;
+	unsigned int len, e_dis = 0, cigar_c, pos, l_indel, l_m1, l_m2;
+  unsigned int *cigar_tmp;
 
-  bamFile fbam = bam_open(argv[3], "r");
-  bam_index_t *idx = bam_index_load(argv[3]);
+  /*bamFile fbam = bam_open(argv[3], "r");
+  bam_index_t *idx = bam_index_load(argv[3]);*/
 
-  Read_collect *rcollection = init_reads();
+  /*Read_collect *rcollection = init_reads();
   extract_read(fbam, idx, tid, beg, end, rcollection);
   printf("number of reads in %d and %d %d\n", beg, end, rcollection->n);
   LOOP(it, 0, rcollection->n) {
     cigar = bam1_cigar(&rcollection->r[it]);
-    DEBUG_CIGAR(cigar, rcollection->r[it].core.n_cigar)
-  }
+		len = rcollection->r[it].core.n_cigar;
+		pos = rcollection->r[it].core.pos;
+		e_dis = 0;
+    DEBUG_CIGAR(cigar, len, i, cigar_c)
+		if(bam_cigar_opchr(cigar[len-1])=='S')
+			{
+				MERGE_SOFTCLIP_RIGHT(cigar, len, e_dis)
+			}
+		if(bam_cigar_opchr(cigar[0])=='S')
+			{
+				MERGE_SOFTCLIP_LEFT(cigar, len, pos, e_dis)
+			}
+		DEBUG_CIGAR(cigar, len, i, cigar_c)
+		printf("\n");
+  }*/
+  unsigned int match = bam_cigar_gen(51, BAM_CMATCH); //800
+  unsigned int softc =  bam_cigar_gen(52, BAM_CSOFT_CLIP); //804
+  unsigned int del =  bam_cigar_gen(53, BAM_CDEL); //802
+  unsigned int ins = bam_cigar_gen(54, BAM_CINS); // 801
+  unsigned int skip = bam_cigar_gen(100, BAM_CREF_SKIP); // 803
+  unsigned int *cigar;
+
+  len = 10;
+  cigar = (unsigned int *)calloc(len, sizeof(unsigned int));
+  memcpy(cigar, (unsigned int[]){match, match, skip, match, ins, match, del, ins, match, softc}, len*sizeof(unsigned int));
+  EDIT_DISTANCE(cigar, len, i , e_dis)
+  DEBUG_CIGAR(cigar, len, i, cigar_c)
+  MERGE_SOFTCLIP_RIGHT(cigar, len, l_m1, l_m2)
+  INDEL_AT_SPLICE(cigar, flag, len, i)
+  printf("indel at splice flag: %d\n", flag);
+  INDEL_RIGHT(cigar, len, i, cigar_c, l_id)
+  printf("indel %s at %d with length %d\n", &cigar_c, i, l_id);
+  MERGE_INDEL(cigar, len, i , l_indel, cigar_c)
+  DEBUG_CIGAR(cigar, len, i, cigar_c)
+  MERGE_MATCH_ITER(cigar, len, i, flag, l_m1, l_m2)
+  DEBUG_CIGAR(cigar, len, i, cigar_c)
+  printf("total edit-distance: %d\n", e_dis);
+  DEBUG(len, i)
+  free(cigar);
   return 0;
 }
